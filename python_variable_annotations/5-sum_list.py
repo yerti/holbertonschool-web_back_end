18d663938efdf333312e0f44bf067e53b37271d7
yerti <yerti0818@gmail.com>
2023-09-25 22:02:23 +0000

    Feat: function that sums a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Script that adds a list of floats."""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of the list of floats."""
+    add = sum(input_list)
+    return(add)
