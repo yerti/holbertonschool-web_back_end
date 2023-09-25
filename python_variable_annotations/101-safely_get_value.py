1a3d1bf379beeb6f996384c1643668aae6e7b24d
yerti <yerti0818@gmail.com>
2023-09-25 23:09:53 +0000

    Feat:script that returns multiple valuedepending on the parameter passed to it

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""script that returns multiple values
+depending on the parameter passed to it"""
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """returns a value from the dictionary if the key is
+    present or the default value if the key is not found in the dictionary"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
