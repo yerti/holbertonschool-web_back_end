77f9556191703d060adac344ee4fea81fdaa37e1
yerti <yerti0818@gmail.com>
2023-09-25 22:06:31 +0000

    Fix: add a blank line using pycodestyle

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """Script that adds a list of floats."""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of the list of floats."""
     add = sum(input_list)
