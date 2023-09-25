21991fe87c5dfe7b7d0475c9aa2883bbbac20316
yerti <yerti0818@gmail.com>
2023-09-25 20:47:21 +0000

    Feat: function that receives a float as an argument and returns a text string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Script that receives a float as an argument and returns it as a string"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns a string."""
+    string = str(n)
+    return(string)
