f18f4d04d34b98d0525c237a5c270a651d3b5b14
yerti <yerti0818@gmail.com>
2023-09-25 20:21:09 +0000

    Feat: function that returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""script that concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concatenated string"""
+    result = str1 + str2
+    return(result)
