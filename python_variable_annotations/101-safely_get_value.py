3b7918b14bb352e6d2b97931673c55d53626c066
yerti <yerti0818@gmail.com>
2023-09-25 23:16:10 +0000

    Fix: modify the function arguments for the pycodestyle style

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,11 +7,14 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+        ) -> Union[Any, T]:
     """returns a value from the dictionary if the key is
     present or the default value if the key is not found in the dictionary"""
     if key in dct:
         return dct[key]
     else:
         return default
-
