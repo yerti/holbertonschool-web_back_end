585546f695e7cb54c4d4e3801cb4c9237126cf53
yerti <yerti0818@gmail.com>
2023-09-25 21:00:42 +0000

    Feat: variables that contain a different data type

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Variables defined with their value type that they save."""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
