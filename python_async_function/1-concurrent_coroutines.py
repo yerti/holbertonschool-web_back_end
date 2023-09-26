b54a2314d951cbb785d7215acd3bf0bd840cd5d2
yerti <yerti0818@gmail.com>
2023-09-26 15:33:32 +0000

    Fix: to add the type of value it receives in the arguments and the value it returns

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """ """
 import asyncio
 import random
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
 
