46bce136e7dfb3a59844bebc3a3b5350b55202c7
yerti <yerti0818@gmail.com>
2023-09-26 15:39:06 +0000

    Fix: add comments to my code

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" """
+"""Imports of the libraries that we are going to use."""
 import asyncio
 import random
 from typing import List
@@ -7,12 +7,15 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Function that returns a list of loads
+    depends on the number of arguments passed to us."""
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
-        result = await task 
+        result = await task
         delays.append(result)
 
     return sorted(delays)
