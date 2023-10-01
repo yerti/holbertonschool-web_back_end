c6a529b812bbd36e0be7cddc7baea888f9488a56
yerti <yerti0818@gmail.com>
2023-10-01 16:59:38 +0000

    Fix: the style pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ function that returns a list of floating random numbers n times """
-    float_list= [task_wait_random(max_delay) for _ in range(n)]
+    float_list = [task_wait_random(max_delay) for _ in range(n)]
 
     delays = [await i for i in asyncio.as_completed(float_list)]
 
