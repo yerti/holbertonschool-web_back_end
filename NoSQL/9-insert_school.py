921132765853ce414e065784ae08258816ba8af8
yerti <yerti0818@gmail.com>
2023-10-05 14:10:49 -0700

    Feat:  Insert a document in Python

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env puthon3
+"""Python function that inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new _id."""
+    return mongo_collection.insert_one(kwargs).inserted_id
