4dc8f7fb6b0c96b8baf671d886026fa6ac5ec632
yerti <yerti0818@gmail.com>
2023-10-05 14:40:54 -0700

    Feat: Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB."""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    db = client.logs
+    col = db.nginx
+
+    num_logs = col.count_documents({})
+
+    print(f"{num_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print("Methods:")
+    for method in methods:
+
+        count = col.count_documents({"method": method})
+
+        print(f"\tmethod {method}: {count}")
+
+    get_status = col.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+
+    print(f"{get_status} status check")
