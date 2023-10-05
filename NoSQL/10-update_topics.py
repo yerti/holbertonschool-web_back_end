26e44e1af6f705a009e6e3854e7cdc1127e447be
yerti <yerti0818@gmail.com>
2023-10-05 14:25:35 -0700

    Feat:Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Python function that changes all topics of a school document based on the name."""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    return mongo_collection.update_many({"name": name}, {"$set":{"topics": topics}})
