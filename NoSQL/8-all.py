df93a6ead2715e839717772f9adf149a0230f2a6
yerti <yerti0818@gmail.com>
2023-10-05 14:03:43 -0700

    Fix: conditional of the function

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,7 +5,7 @@ import pymongo
 
 def list_all(mongo_collection):
     """Lists all documents in a collection."""
-    if mongo_collection is not None:
+    if mongo_collection is None:
         return []
 
     return list(mongo_collection.find())
