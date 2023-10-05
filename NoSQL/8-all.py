f57a698255e1c9bf97d2abb389d2bb07e835aa21
yerti <yerti0818@gmail.com>
2023-10-05 14:01:36 -0700

    Feat:  List all documents in Python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python function that lists all documents in a collection"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection."""
+    if mongo_collection is not None:
+        return []
+
+    return list(mongo_collection.find())
