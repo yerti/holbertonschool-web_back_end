368ffb6b786bbf22e811882b1e2fde17f8199795
yerti <yerti0818@gmail.com>
2023-10-14 18:54:45 -0500

    Fix: correction of ESLint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from "./0-classroom";
 
 export default function initializeRooms() {
-    let room1 = new ClassRoom(19);
-    let room2 = new ClassRoom(20);
-    let room3 = new ClassRoom(34);
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
 
-    return [room1, room2, room3];
+  return [room1, room2, room3];
 }
