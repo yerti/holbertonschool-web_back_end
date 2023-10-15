795a94e1329a47f97532bde0915a4fa4e088725d
yerti <yerti0818@gmail.com>
2023-10-15 15:56:47 -0500

    Feat: Implement a class named Airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(newCode) {
+    this._code = newCode
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
