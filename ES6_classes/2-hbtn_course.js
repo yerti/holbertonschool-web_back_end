b75bcbe5bcd680905cb6c61b3777c6ce72dd4a68
yerti <yerti0818@gmail.com>
2023-10-14 19:32:05 -0500

    Feat: create a class called HolbertonCourse

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+// creat a class of name HolbertonCourse
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw TypeError('Students must be an array');
+
+    // Attribute
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+  
+  // Setter fro name
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+  
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+  
+  // Setter for length 
+  set length(newLength) {
+    if (typeof newLength !== 'number') throw TypeError('Length must be a number');
+    this._length = newLength;
+  }
+  
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+  
+  // Setter for students
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) throw TypeError('Students must be an array');
+    this._students = newStudents;
+  }
+}
