b928a13b9ea9215063a5fb54c4a15b181213cb0e
yerti <yerti0818@gmail.com>
2023-10-28 16:43:18 -0500

    Fix: modify the spaces

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,11 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        if (true) {
-            resolve();
-        } else {
-            reject();
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 };
 
 export default getResponseFromAPI;
