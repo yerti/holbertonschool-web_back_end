5dca61273e5c745d3ffa1347fbfd0fef8a5cda77
yerti <yerti0818@gmail.com>
2023-10-29 16:55:20 -0500

    Feat: function return an array

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+export default function guardrail(mathFunction) {
+  // function return an array 
+  const queue = [];
+  let result;
+
+  try {
+    result = mathFunction();
+  } catch (e) {
+    result = e.toString();
+  }
+
+  queue.push(result);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
