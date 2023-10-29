87d20153f553cbd3e27f2f1e0526e9c50134a88f
yerti <yerti0818@gmail.com>
2023-10-29 00:49:45 -0500

    Fix: modify file

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,19 @@
 import singUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), singUpUser(firstName, lastName)]);
-}
+const handleProfileSignup = async (firstName = '', lastName = '', fileName = '') => {
+  const newResult = [];
+  try {
+    const user = await singUpUser(firstName, lastName);
+    newResult.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    newResult.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return newResult;
+};
+
+export default handleProfileSignup;
