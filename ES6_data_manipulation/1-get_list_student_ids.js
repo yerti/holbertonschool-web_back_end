f13dfbb7de47ba759ee30d70a2024a6742f6647f
yerti <yerti0818@gmail.com>
2023-10-29 18:01:11 -0500

    Feat: returns an array of ids from a list of object

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentsIds(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+
+    const ids = students.map(student => student.id);
+
+    return ids
+}
