8ba90641b23a0bf7f997552d7908126a6040d115
yerti <yerti0818@gmail.com>
2023-10-29 19:21:09 -0500

    Feat:  returns an array of objects who are located in a specific city

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+export default function getStudentsByLocation(students, city) {
+  // returns an array of objects who are located in a specific city
+  const location = students.filter(
+    (student) => student.location === city,
+  );
+
+  return location
+}
